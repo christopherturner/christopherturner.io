9410a0a501ec0cb2473e76d14f3d8b42e6d37c00
Christopher Turner <2086667+christopherturner@users.noreply.github.com>
2025-09-16 21:29:45 -0400

    Added Home component (w/ Hello)

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { AbsoluteCenter, Heading, Stack } from "@chakra-ui/react";
+
+const Home = () => (
+    <AbsoluteCenter>
+        <Stack>
+            <Heading size="6xl">
+                Hello
+            </Heading>
+        </Stack>
+    </AbsoluteCenter>
+);
+
+export default Home;
